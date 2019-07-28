0cd5cc1d5ffa67a5760909864d1975f095cad11d
Jorge <stivenflo1993@gmail.com>
2019-07-28 17:41:55 -0600

    commit

---
+++ Amorem Artis/Amorem Artis/UserControlSeccionAgregar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Amorem_Artis
+{
+    /// <summary>
+    /// Lógica de interacción para UserControlSeccionAgregar.xaml
+    /// </summary>
+    public partial class UserControlSeccionAgregar : UserControl
+    {
+        public UserControlSeccionAgregar()
+        {
+            InitializeComponent();
+        }
+    }
+}
