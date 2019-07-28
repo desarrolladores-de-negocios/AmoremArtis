f3e285426a49e986950597f22724c5efc3af77a5
athroscf <fialloschris1@gmail.com>
2019-07-27 21:21:10 -0600

    Completacion de las opciones del menu principal.

--- Amorem Artis/Amorem Artis/MainWindow.xaml.cs
+++ Amorem Artis/Amorem Artis/MainWindow.xaml.cs
@@ -27,15 +27,37 @@ namespace Amorem_Artis
         {
             InitializeComponent();
 
-            var menuRegister = new List<SubItem>();
-            menuRegister.Add(new SubItem("Instrucciones", new UserControlInstrumentos()));
-            menuRegister.Add(new SubItem("Secciones"));
-            menuRegister.Add(new SubItem("Sulfeo"));
-            var item1 = new ItemMenu("Cursos", menuRegister, PackIconKind.Class);
+            var menuCursos = new List<SubItem>();
+            menuCursos.Add(new SubItem("Instrumentos", new UserControlInstrumentos()));
+            menuCursos.Add(new SubItem("Secciones"));
+            menuCursos.Add(new SubItem("Solfeo"));
+            var item1 = new ItemMenu("Cursos", menuCursos, PackIconKind.Piano);
 
+            var menuAlumnos = new List<SubItem>();
+            menuAlumnos.Add(new SubItem("Alumno", new UserControlInstrumentos()));
+            menuAlumnos.Add(new SubItem("Secciones"));
+            menuAlumnos.Add(new SubItem("Padres"));
+            menuAlumnos.Add(new SubItem("Solfeo"));
+            menuAlumnos.Add(new SubItem("Pagos"));
+            var item2 = new ItemMenu("Alumnos", menuAlumnos, PackIconKind.Person);
+
+            var menuMaestros = new List<SubItem>();
+            menuMaestros.Add(new SubItem("Maestros", new UserControlInstrumentos()));
+            menuMaestros.Add(new SubItem("Secciones"));
+            menuMaestros.Add(new SubItem("Solfeo"));
+            menuMaestros.Add(new SubItem("Pagos"));
+            var item3 = new ItemMenu("Maestros", menuMaestros, PackIconKind.Teacher);
+
+            var menuUsuarios = new List<SubItem>();
+            menuUsuarios.Add(new SubItem("Ver Usuarios", new UserControlInstrumentos()));
+            menuUsuarios.Add(new SubItem("Agregar Usuario"));
+            menuUsuarios.Add(new SubItem("Quitar Usuario"));
+            var item4 = new ItemMenu("Usuarios", menuUsuarios, PackIconKind.User);
 
             Menu.Children.Add(new UserControlMenuItem(item1, this));
-  
+            Menu.Children.Add(new UserControlMenuItem(item2, this));
+            Menu.Children.Add(new UserControlMenuItem(item3, this));
+            Menu.Children.Add(new UserControlMenuItem(item4, this));
         }
 
         internal void SwitchScreen(object sender)
