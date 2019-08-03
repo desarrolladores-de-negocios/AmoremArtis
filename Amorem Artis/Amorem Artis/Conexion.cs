0ac20aa4df0f387812632152dcd1e4d905e7afd5
athroscf <fialloschris1@gmail.com>
2019-08-03 11:24:19 -0600

    Creacion del origen de datos y las clases Encriptacion y Conexion.

---
+++ Amorem Artis/Amorem Artis/Conexion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Amorem_Artis
+{
+    class Conexion
+    {
+        SqlConnection connectionString = new SqlConnection(@"server = (local)\sqlexpress; database = AmoremArtis; integrated security = true");
+
+        public void AbrirConexion()
+        {
+            try
+            {
+                // Abrir la conexión
+                connectionString.Open();
+            }
+            catch (SqlException ex)
+            {
+                // Desplegar la causa del error (excepción)
+                Console.WriteLine("Error: " + ex.Message + ex.StackTrace);
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            try
+            {
+                connectionString.Close();
+            }
+            catch (SqlException ex)
+            {
+                // Desplegar la causa del error (excepción)
+                Console.WriteLine("Error: " + ex.Message + ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ Amorem Artis/Amorem Artis/Encriptacion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Amorem_Artis
+{
+    class Encriptacion
+    {
+        public string laCadena { get; set; }
+
+        public static string procesarSha256Hash(string laCadena)
+        {
+            // Crear SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Obtener el hash 
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(laCadena));
+
+                // Convertir arreglo de bytes a string 
+                StringBuilder constructor = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    constructor.Append(bytes[i].ToString("x2"));
+                }
+                return constructor.ToString();
+            }
+        }
+    }
+}
